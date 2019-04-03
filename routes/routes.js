b4cff8643838bd0da38f1a46fb0520c7a5f67eae
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-04-03 14:37:34 +0200

    Added Login functionality. Token to be implemented

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Users = require('./models.js')
 
+// No need to pre-pend '/api'
 router.post('/register', (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 12)
@@ -17,9 +18,26 @@ router.post('/register', (req, res) => {
       res.status(500).json(error)
     })
 })
-// router.post('/api/register')
 
 // router.post('/api/login')
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({
+            message: `Welcome ${user.username}`
+          })
+        } else {
+          res.status(401).json({ message: "Your username or password are incorrect"})
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error)
+      })
+})
 
 // router.get('/api/users')
 
